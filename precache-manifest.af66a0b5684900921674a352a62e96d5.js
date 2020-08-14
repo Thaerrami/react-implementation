3fa5423075d97c58d134dc4bf1dec10669442f27
unknown <thaer.rami99.alazzeh@gmail.com>
2020-08-14 07:29:24 +0300

    Updates

---
+++ precache-manifest.af66a0b5684900921674a352a62e96d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e587cf7e3c6955b4586088d88452df",
+    "url": "/react-implementation/index.html"
+  },
+  {
+    "revision": "a8a250871ae97a6f8493",
+    "url": "/react-implementation/static/css/main.a3201ada.chunk.css"
+  },
+  {
+    "revision": "3be9a7eccaf8035ad5e0",
+    "url": "/react-implementation/static/js/2.4be2965b.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/react-implementation/static/js/2.4be2965b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a250871ae97a6f8493",
+    "url": "/react-implementation/static/js/main.856f5c71.chunk.js"
+  },
+  {
+    "revision": "aa362c279b5f02130e20",
+    "url": "/react-implementation/static/js/runtime-main.ded163ab.js"
+  }
+]);
